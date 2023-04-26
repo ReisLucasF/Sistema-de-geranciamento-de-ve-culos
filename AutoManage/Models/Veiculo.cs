afe5f0da2d5c092067facf857be7a3f94b9529cc
Lucas Reis <lucasfilipe224@gmail.com>
2023-04-25 22:38:55 -0300

    Feat: Adição do model Veículo

---
+++ AutoManage/Models/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoManage.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="É obrigatório informar o nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório informar a placa")]
+        public string Placa { get; set; }
+    }
+}
